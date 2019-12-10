2c3743872297c9cc99e7fd042a7799bb32e1f99a
maartenks <mjjkesse@avans.nl>
2019-12-10 21:20:41 +0100

    waypoint

--- Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/data/Waypoint.java
+++ Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/data/Waypoint.java
@@ -1,9 +1,75 @@
 package com.example.breda_op_stap.data;
 
+import android.provider.MediaStore;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
 public class Waypoint
 {
-    public Waypoint()
-    {
+    LatLng location;
+    String description;
+    List<MediaStore.Images> images;
+    Boolean isVisited;
+    Boolean isFavorite;
+    Boolean isHidden;
 
+    public Waypoint(LatLng position, String desc, List<MediaStore.Images> images, Boolean isVisited, Boolean isFavorite, Boolean isHidden )
+    {
+        this.location = position;
+        this.description = desc;
+        this.images = images;
+        this.isVisited = isVisited;
+        this.isFavorite = isFavorite;
+        this.isHidden = isHidden;
+    }
+
+    public Boolean getFavorite() {
+        return isFavorite;
+    }
+
+    public Boolean getHidden() {
+        return isHidden;
+    }
+
+    public Boolean getVisited() {
+        return isVisited;
+    }
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public List<MediaStore.Images> getImages() {
+        return Images;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public void setHidden(Boolean hidden) {
+        isHidden = hidden;
+    }
+
+//    public void setImages(List<MediaStore.Images> images) {
+//        List<MediaStore.Images> = images;
+//    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
+
+    public void setVisited(Boolean visited) {
+        isVisited = visited;
     }
 }
