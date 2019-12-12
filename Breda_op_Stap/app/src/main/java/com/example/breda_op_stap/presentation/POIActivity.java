8b2ec26ce25bb1e35fb9535d88d1ea1071f74f49
maartenks <mjjkesse@avans.nl>
2019-12-12 14:52:28 +0100

    Fixed errors

--- Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/presentation/POIActivity.java
+++ Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/presentation/POIActivity.java
@@ -11,6 +11,7 @@ import com.example.breda_op_stap.R;
 import com.example.breda_op_stap.data.Waypoint;
 import com.example.breda_op_stap.logic.POIAdapter;
 import com.example.breda_op_stap.logic.POIListener;
+import com.example.breda_op_stap.logic.RouteParser;
 
 import java.util.ArrayList;
 
