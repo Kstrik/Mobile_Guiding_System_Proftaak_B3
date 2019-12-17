d79fbfef16c016a7b385da90b65b6dc66923d712
StinovB <sj.vanberkel@student.avans.nl>
2019-12-17 12:06:53 +0100

    added backbutton in detailview
    added backbutton and you're now able to scroll the textview so you can read the whole description

--- Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/presentation/DetailedActivity.java
+++ Breda_op_Stap/app/src/main/java/com/example/breda_op_stap/presentation/DetailedActivity.java
@@ -2,7 +2,12 @@ package com.example.breda_op_stap.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,9 +27,17 @@ public class DetailedActivity extends AppCompatActivity
         TextView title = this.findViewById(R.id.poiTitle);
         TextView description = this.findViewById(R.id.poiInfo);
         ImageView image = this.findViewById(R.id.pointOfInterest_img);
+        ImageView back = this.findViewById(R.id.backArrow);
+
+        TextView scrollable = this.findViewById(R.id.poiInfo);
+
+        //Enabling scrolling on TextView.
+        scrollable.setMovementMethod(new ScrollingMovementMethod());
 
         title.setText(waypoint.getName());
         description.setText(waypoint.getDescription());
+
+        back.setOnClickListener(v -> startActivity(new Intent(DetailedActivity.this, GoogleMapsActivity.class)));
 
         String path = "@drawable/" + waypoint.getImages().get(0);
         int res = getResources().getIdentifier(path, null, getPackageName());
